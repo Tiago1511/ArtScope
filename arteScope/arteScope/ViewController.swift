d1864e4f2722c837f11f74700913d585108681e3
tiago <tiagoribeiro15111@gmail.com>
2026-01-01 15:47:12 +0000

    create project
    - min version: 17
    - add CoreData

---
+++ arteScope/arteScope/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  arteScope
+//
+//  Created by tiago on 01/01/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
