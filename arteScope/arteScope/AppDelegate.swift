b2ab6b1e97bc54ad78e1b763f351735b83cd5877
tiago <tiagoribeiro15111@gmail.com>
2026-02-02 16:56:52 +0000

    add constraint in coreData
    no add duplicated arts in coreData
    add app icons

--- arteScope/arteScope/AppDelegate.swift
+++ arteScope/arteScope/AppDelegate.swift
@@ -58,6 +58,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+        container.viewContext.mergePolicy =
+                NSMergeByPropertyObjectTrumpMergePolicy
         return container
     }()
 
